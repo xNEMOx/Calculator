c626d4bdb00742d0f4646e9ac3e4e9873b49e025
Nemo <63228128+xNEMOx@users.noreply.github.com>
2021-10-05 11:41:54 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# simple calculator by xNEMOx
+
+# asking for the numbers and symbols
+number1 = input("Enter your first number: ")
+symbol = input("Enter your symbol: ")
+number2 = input("Enter your 2nd number: ")
+answer = print("Your answer is: ")
+
+
+# calculating process
+if symbol == "+":
+    print(int(number1) + int(number2))
+elif symbol == "-":
+    print(int(number1) - int(number2))
+elif symbol == "*":
+    print(int(number1) * int(number2))
+elif symbol == "/":
+    print(int(number1) / int(number2))
